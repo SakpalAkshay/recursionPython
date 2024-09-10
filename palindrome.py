fce498a3ddfda7ba948c2f272c3b1314d8b0e8c8
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-09-10 15:30:26 -0700

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,1 +1,13 @@
+#palindrome checker using recursion
+string = "madams"
+n = len(string)
+def palindrome(i):
+  if i>=n/2:
+    return True
+  if string[i]!=string[n-i-1]:
+    return False
+  return palindrome(i+1)
+ans = palindrome(0)
+print(ans)
+
 
