e98d1568392115badeb07849325b90d6d7ff5413
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-09-11 13:06:45 -0700

    Update reverseArray.py

--- reverseArray.py
+++ reverseArray.py
@@ -1,1 +1,9 @@
+arr = [1,2,3,4,5,6,0]
 
+def reverse(arr,l,r):
+  if l>r: #base condition
+    return 
+  arr[l], arr[r] = arr[r], arr[l]
+  reverse(arr,l+1,r-1)
+reverse(arr,0,len(arr)-1)
+print(arr)
