87e5a435c711273e9eb135953d27f960e9cd41ae
Akshay Sakpal <131487996+SakpalAkshay@users.noreply.github.com>
2024-09-15 14:21:52 -0700

    pushed code

--- faith-expectations-approach/increasing.py
+++ faith-expectations-approach/increasing.py
@@ -1,1 +1,16 @@
+#lets work with Expectation, Faith and connecting expectation with faith
 
+#print Increasing Order 1 to N
+
+#Expectation f(n) => will print 1,2,3,4 ..... till n
+#Faith f(n-1) => will print 1,2,3, .... till n - 1
+#Connecting Expectation with Faith f(n) =  f(n - 1) => will print from 1 to n - 1 and then we print(n)
+
+def increasing(n):
+  if n==0:
+    return
+  increasing(n-1) #this will print 1 to n - 1
+  print(n)
+
+
+increasing(5)
